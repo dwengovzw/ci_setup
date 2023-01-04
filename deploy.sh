e473313260c1da513637cb543dd301ad78bc4afb
Tom Neutens <tomneutens@gmail.com>
2023-01-04 17:55:29 +0100

    updated jenkinsfile

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,13 @@
 
 # Build the required docker containers
 #docker build -t secure_web_server ./apache_ssl  # Web server with ssl
-docker build -t web_server ./apache  # Web server without ssl
-docker build -t jenkins_server ./jenkins  # Build jenkins container
-docker build -t mongodb_server ./mongodb  # Build jenkins container
+cd apache
+docker build -t web_server .  # Web server without ssl
+cd ..
+cd jenkins
+docker build -t jenkins_server .  # Build jenkins container
+cd ..
+cd mongodb
+docker build -t mongodb_server .  # Build jenkins container
+cd ..
 docker-compose up -d  # Start containers in deamon mode
