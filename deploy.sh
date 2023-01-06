d92802f644d50b96652545978713fd843b8fad97
Tom Neutens <tomneutens@gmail.com>
2023-01-06 14:31:23 +0100

    Clead up deploy script, added new jenkinsfile for staging and renamed old jenkinsfile, added command line parameter to docker_deploy.sh script in deploy stage

--- deploy.sh
+++ deploy.sh
@@ -9,16 +9,12 @@ cp ./environments/* /var/environments/
 # Build the required docker containers
 #docker build -t secure_web_server ./apache_ssl  # Web server with ssl
 cd apache
-docker build -t web_server .  # Web server without ssl
-cd ..
-cd jenkins
+docker build -t web_server ./apache  # Web server without ssl
 # Pass groupid of docker group to jenkins image so it can access the host docker socket
-docker build --build-arg DOCKERGID=`stat -c %g /var/run/docker.sock` -t jenkins_server .  # Build jenkins container
-cd ..
-cd mongodb
-docker build -t mongodb_server .  # Build jenkins container
-cd ..
-cd blockly_development
-docker build -t blockly_development_server .  # Build jenkins container
-cd ..
+docker build --build-arg DOCKERGID=`stat -c %g /var/run/docker.sock` -t jenkins_server ./jenkins  # Build docker container for jenkins
+docker build -t mongodb_server ./mongodb  # Build docker container for database
+
+docker build -t blockly_development_server ./blockly_development  # Build image for development deploy
+docker build -t blockly_staging_server ./blockly_development  # Build image for staging deploy
+
 docker-compose up -d  # Start containers in deamon mode
