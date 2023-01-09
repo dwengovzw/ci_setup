624d669a46f54de627aad592f6fce8aa44c1f0ea
Tom Neutens <tomneutens@gmail.com>
2023-01-09 10:35:27 +0100

    Updated config for staging server

--- deploy.sh
+++ deploy.sh
@@ -7,13 +7,13 @@
 cp ./environments/* /var/environments/
 
 # Build the required docker containers
-#docker build -t secure_web_server ./apache_ssl  # Web server with ssl
-docker build -t web_server ./apache  # Web server without ssl
+docker build --network host -t secure_web_server ./apache_ssl  # Web server with ssl
+#docker build --network host -t web_server ./apache  # Web server without ssl
 # Pass groupid of docker group to jenkins image so it can access the host docker socket
-docker build --build-arg DOCKERGID=`stat -c %g /var/run/docker.sock` -t jenkins_server ./jenkins  # Build docker container for jenkins
-docker build -t mongodb_server ./mongodb  # Build docker container for database
+docker build --network host --build-arg DOCKERGID=`stat -c %g /var/run/docker.sock` -t jenkins_server ./jenkins  # Build docker container for jenkins
+docker build --network host -t mongodb_server ./mongodb  # Build docker container for database
 
-docker build -t blockly_development_server ./blockly_development  # Build image for development deploy
-docker build -t blockly_staging_server ./blockly_development  # Build image for staging deploy
+docker build --network host -t blockly_development_server ./blockly_development  # Build image for development deploy
+docker build --network host -t blockly_staging_server ./blockly_development  # Build image for staging deploy
 
 docker-compose up -d  # Start containers in deamon mode
